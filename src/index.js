4345620f248c6f59d4a9f8178e2c65d2b4cc31c0
Percival Aceron <perci.aceron@gmail.com>
2024-02-04 15:51:57 +0800

    cat-api

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
-import SlimSelect from 'slim-select';
 import Notiflix from 'notiflix';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const apiKey =
   'live_6h4QUxeTfC4qVpMQNe7IsH0bXSFGfs90JToDMxK4F4XoDf10nSigsOxUm0TBw1hS';
 
-const breedSelect = new SlimSelect({
-  select: '.breed-select',
-});
-
+const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 
-// Access the original select element from SlimSelect
-const slimSelectElement = breedSelect.data.select;
-
-// Add event listener for SlimSelect change event
-slimSelectElement.addEventListener('change', event => {
+// Add event listener for select element change event
+breedSelect.addEventListener('change', event => {
   const selectedBreedId = event.target.value;
   fetchCatByBreed(apiKey, selectedBreedId);
 });
